dac957d7d8dc637566fe9879a887dd50932dde74
thinhnc99 <ncthinh.199@gmail.com>
2022-11-20 16:18:44 +0700

    install bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const User = require('../model/User')
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const { registerValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -12,11 +13,16 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
 
+    // Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        // password: req.body.password password chưa mã hóa
+        password: hashedPassword
     });
     try {
         const savedUser = await user.save();
