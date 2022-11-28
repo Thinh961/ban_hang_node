eee462dd5f7446352ca186b6e5f9105f2862a9b4
Thinh961 <ncthinh.199@gmail.com>
2022-11-29 00:05:10 +0700

    fix & update

--- routes/posts.js
+++ routes/posts.js
@@ -10,7 +10,7 @@ router.get('/', verify, (req, res) => {
     //     }
     // })
     res.send(req.user);
-    User.findbyOne({_id: req.user})
+    User.findById({_id: req.user})
 })
 
 module.exports = router;
