a72de2bcb0186bd36db896cd4245278cb8b88390
thinhnc <ncthinh.199@gmail.com>
2022-11-28 18:02:08 +0700

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const bcrypt = require('bcryptjs');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
 
+// register
 router.post('/register', async (req, res) => {
 
     // Lets validate the data before we a user
@@ -26,10 +27,24 @@ router.post('/register', async (req, res) => {
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user: user._id});
     } catch (err) {
         res.status(400).send(err);
     }
 });
 
+// Login
+router.post('/login', async (req, res) => {
+    // let validate the data before we a user
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    // Checking if the email exists
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email is not found');
+    // is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid password')
+
+    res.send('Login in!');
+});
 module.exports = router;
